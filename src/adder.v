0d54c697ff0aa371b29cb5c019b22e02206aef22
aaneriC <carabit@cpp.edu>
2026-03-09 10:11:22 -0700

    Change adder to use ONE instead of ALL_ONES

--- src/adder.v
+++ src/adder.v
@@ -2,8 +2,8 @@
 module adder(pc_in, adder_out);
     input [31:0] pc_in;
     output [31:0] adder_out;
-    localparam [31:0] ALL_ONES = 32'hFFFFFFFF;
+    localparam [31:0] ONE = 32'd1;
     
-    assign adder_out = pc_in + ALL_ONES;
+    assign adder_out = pc_in + ONE;
     
 endmodule
